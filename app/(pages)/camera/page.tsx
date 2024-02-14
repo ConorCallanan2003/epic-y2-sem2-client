5707e596f0891feaa18e8b5b38e07a1fdf46401d
ConorCallanan2003 <conorcallanan03@gmail.com>
2024-02-14 21:45:17 +0000

    Testing deployment

--- app/(pages)/camera/page.tsx
+++ app/(pages)/camera/page.tsx
@@ -2,19 +2,5 @@ import S3, { ObjectList } from "aws-sdk/clients/s3";
 import { useEffect, useState } from "react";
 
 export default function Camera() {
-  // const bucketName = "epic-y2-sem3-image-bucket";
-  // const s3 = new S3();
-
-  // const [files, setFiles] = useState<ObjectList>([]);
-
-  // useEffect(() => {
-  //   const fetchFiles = async () => {
-  //     const response = await s3.listObjectsV2({ Bucket: bucketName }).promise();
-  //     setFiles(response.Contents!);
-  //   };
-
-  //   fetchFiles();
-  // }, []);
-
   return <div></div>;
 }
