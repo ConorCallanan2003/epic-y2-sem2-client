8658d9153038892ee1b7e512558aeabd8dc94f9f
ConorCallanan2003 <conorcallanan03@gmail.com>
2024-03-22 10:19:31 +0000

    Submit form -> Submit feedback

--- app/(pages)/(wNavBar)/support/page.tsx
+++ app/(pages)/(wNavBar)/support/page.tsx
@@ -8,7 +8,7 @@ export default function Support() {
       <h2 className="text-3xl font-light">We&apos;re sorry to hear it</h2>
       <div className="w-full xl:w-2/3 h-full grid gap-8 place-items-center md:grid-cols-2 pt-8">
         <div className="h-[400px] lg:w-[400px] w-[300px] rounded-lg border-2 border-white flex flex-col items-center">
-          <h1 className="pt-6 text-3xl font-bold">Submit form</h1>
+          <h1 className="pt-6 text-3xl font-bold">Submit feedback</h1>
           <Send className="h-full" size={160} />
           <Link href={"/support/submit-form"}>
             <button className="border-2 border-white font-medium duration-200 hover:scale-105 hover:bg-white hover:text-black rounded-lg px-4 py-2 mb-6">
